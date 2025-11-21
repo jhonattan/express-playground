ca9dcd8ee89442b40d57ef3c3cf2ea718f7928a6
Jota <jsaldana999@gmail.com>
2025-11-20 21:14:47 -0500

    Refactor request handlers and error handling in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,51 @@
-import express from "express";
+import express, { RequestHandler, ErrorRequestHandler } from "express";
 import { pool } from "./db";
+//import { nextTick } from "process";
 
 const app = express();
 const port = 3000;
 
-app.get("/users", async (_req, res) => {
+const handler1: RequestHandler = (_req, _res, next) => {
+  console.log("Primer handler");
+  next("route");
+};
+
+const handler2: RequestHandler = (_req, _res, next) => {
+  console.log("Segundo handler");
+  next();
+};
+
+const handler3: RequestHandler = (_req, res) => {
+  console.log("Tercer handler");
+  res.send("Finaliza el ciclo solicitud-respuesta");
+};
+
+app.get("/users", async (_req, res, next) => {
   try {
     const result = await pool.query("SELECT id FROM users limit 1;");
     res.status(201).json(result.rows);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Error fetching users" });
+    next(err);
+
+    // console.error(err);
+    // res.status(500).json({ message: "Error fetching users" });
   }
 });
 
-// app.get("/users/:userId", (req, res) => {
-//   const userId = req.params.userId;
+app.get("/", handler1, handler2);
+app.get("/", handler3);
 
-//   //go to BD and get user data with userId
+app.get("/error", (_req, _res, next) => {
+  const error = new Error("This is a test error!");
+  next(error); // Pass error to error handler
+});
 
-//   console.log(`${req.method} ${req.url}`);
-//   res.json("Hola Mundo con Express y TypeScript!");
-// });
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).send("Algo salio mal");
+};
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
