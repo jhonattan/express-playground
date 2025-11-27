ab72f1d4d60b9f46500575f981263c8aab5e0481
Jota <jsaldana999@gmail.com>
2025-11-26 21:06:55 -0500

    feat: implement JWT authentication and user session management

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,57 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import bcrypt from "bcrypt";
 import session from "express-session";
 import connect from "connect-pg-simple";
+import jwt from "jsonwebtoken";
 import { pool } from "./db";
 import { createUser, getUser, getUserByEmail } from "./data/user-data";
 
 const app = express();
 const port = 5500;
 
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: number;
+    }
+  }
+}
+
 declare module "express-session" {
   interface SessionData {
     userId: string;
+  }
+}
+
+const jwtSecret = "ultra-secret";
+
+export function authenticateHandler(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) {
+  // if (!token) {
+  //   return next(new ApiError("No autorizado", 401));
+  // }
+
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return;
+    }
+
+    const payload = jwt.verify(token, jwtSecret) as {
+      userId: number;
+      iat: number;
+      exp: number;
+    }; // { userId: 5, iat: 1704896639, exp: 1704896699 }
+
+    req.userId = payload.userId;
+    next();
+  } catch (error) {
+    return;
   }
 }
 
@@ -74,31 +114,89 @@ app.post("/login", sessionMiddleware, async (req, res) => {
 
   const isValid = await bcrypt.compare(password, user.password);
   if (isValid) {
+    const payload = { userId: user.id };
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: "1m" });
+
+    res.json({ ok: true, message: "Login exitoso", data: { token } });
+
     // Guardamos el id del usuario en la sesión
-    req.session.userId = user.id;
-    res.send("Login exitoso");
+    //req.session.userId = user.id;
+    //res.send("Login exitoso");
   } else {
     res.status(401).send("Credenciales incorrectas");
   }
 });
 
-app.get("/user", sessionMiddleware, async (req, res) => {
-  // leemos el id del usuario desde la sesión
-  const userId = req.session.userId;
+// app.get("/user", sessionMiddleware, async (req, res) => {
+//   const token = req.headers.authorization?.split(" ")[1] || "";
+//   let userId;
 
-  //const user = users.find((u) => u.id === userId);
+//   try {
+//     const payload = jwt.verify(token, jwtSecret) as {
+//       userId: number;
+//       iat: number;
+//       exp: number;
+//     }; // { userId: 5, iat: 1704896639, exp: 1704896699 }
 
-  if (!userId) {
-    res.status(403).send("Acceso denegado");
-    return;
-  }
+//     userId = payload.userId;
+//   } catch (error) {
+//     //next(new ApiError("No autorizado", 401));
+//     return;
+//   }
 
-  const user = await getUser(Number(userId));
+//   if (!userId) {
+//     //next(new ApiError("No autorizado", 401));
+//     return;
+//   }
 
+//   const user = await getUser(userId);
+//   if (user) {
+//     res.json(user);
+//   } else {
+//     //next(new ApiError("No autorizado", 401));
+//     return;
+//   }
+
+//   // leemos el id del usuario desde la sesión
+//   //const userId = req.session.userId;
+
+//   //const user = users.find((u) => u.id === userId);
+
+//   // if (!userId) {
+//   //   res.status(403).send("Acceso denegado");
+//   //   return;
+//   // }
+
+//   // const user = await getUser(Number(userId));
+
+//   // if (user) {
+//   //   res.json(user);
+//   // } else {
+//   //   res.status(403).send("Acceso denegado");
+//   // }
+// });
+
+app.post("/logout", sessionMiddleware, (req, res) => {
+  // Borramos la sesión del lado del servidor
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Error al cerrar sesión");
+      return;
+    }
+    // Opcionalmente, borramos la sesión del lado del cliente también
+    res.clearCookie("connect.sid");
+    res.send("Logout exitoso");
+  });
+});
+
+app.get("/user2", authenticateHandler, async (req, res) => {
+  const user = await getUser(Number(req.userId));
   if (user) {
     res.json(user);
   } else {
-    res.status(403).send("Acceso denegado");
+    //next(new ApiError("No autorizado", 401));
+    return;
   }
 });
 
